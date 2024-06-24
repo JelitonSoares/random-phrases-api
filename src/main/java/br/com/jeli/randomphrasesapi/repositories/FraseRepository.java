e44c73d75445d49d9d27001463980c37f974e723
JelitonSoares <jelitonn.soarres@gmail.com>
2024-06-24 20:03:38 -0300

    feat: 'Implementando o método para buscar registros aleatórios no banco de dados'

--- src/main/java/br/com/jeli/randomphrasesapi/repositories/FraseRepository.java
+++ src/main/java/br/com/jeli/randomphrasesapi/repositories/FraseRepository.java
@@ -2,6 +2,12 @@ package br.com.jeli.randomphrasesapi.repositories;
 
 import br.com.jeli.randomphrasesapi.models.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
+
+    @Query("SELECT f FROM Frase f ORDER BY FUNCTION('RANDOM') LIMIT 1")
+    Frase pegarFrasesAleatorias();
 }
