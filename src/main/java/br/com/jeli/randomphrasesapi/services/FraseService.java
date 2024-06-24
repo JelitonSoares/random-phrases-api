7fbd8c47f2e2fcae010a95f4c3d6656e0032a509
JelitonSoares <jelitonn.soarres@gmail.com>
2024-06-24 20:04:57 -0300

    feat: 'Implementando o método que vai pegar um dado do banco e retornar um DTO'

--- src/main/java/br/com/jeli/randomphrasesapi/services/FraseService.java
+++ src/main/java/br/com/jeli/randomphrasesapi/services/FraseService.java
@@ -1,4 +1,20 @@
 package br.com.jeli.randomphrasesapi.services;
 
+import br.com.jeli.randomphrasesapi.dtos.FraseDTO;
+import br.com.jeli.randomphrasesapi.models.Frase;
+import br.com.jeli.randomphrasesapi.repositories.FraseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FraseService {
+    @Autowired
+    private FraseRepository repository;
+
+
+    public FraseDTO pegarFrasesAleatorias() {
+        Frase frase = this.repository.pegarFrasesAleatorias();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
+    }
+
 }
