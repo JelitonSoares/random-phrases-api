d4c61ffba29dfe2f6c540ae1c7973b5ce88a9fe3
JelitonSoares <jelitonn.soarres@gmail.com>
2024-06-24 19:30:37 -0300

    Upando o projeto

---
+++ src/main/java/br/com/jeli/randomphrasesapi/RandomphrasesapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeli.randomphrasesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomphrasesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomphrasesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeli/randomphrasesapi/RandomphrasesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeli.randomphrasesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomphrasesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
